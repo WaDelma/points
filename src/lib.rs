6e31462f0205167fde5b5ee434a9914aaa7f6703
WaDelma <delma@wa>
2015-10-13 20:42:03 +0300

    Reimplmented using macro_rules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,50 @@
-#![crate_type="dylib"]
-#![feature(plugin_registrar, rustc_private, box_syntax)]
+#[macro_use]
+extern crate lazy_static;
 
-extern crate syntax;
-extern crate rustc;
-
-use syntax::ast::{Ident, Mod, MetaItem};
-use syntax::ast::Item_::{ItemMod, ItemFn};
-use syntax::ast::MetaItem_::{MetaNameValue};
-use syntax::ast::Lit_::{LitStr};
-use syntax::parse::token::{intern, InternedString};
-use syntax::ext::base::{ExtCtxt, MultiDecorator, Annotatable};
-use syntax::ext::base::Annotatable::Item;
-use syntax::codemap::Span;
-use syntax::ptr::P;
-
-use rustc::plugin::Registry;
-
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::Write;
-use std::cell::RefCell;
-
-#[plugin_registrar]
-pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_syntax_extension(intern("points"), MultiDecorator(box do_it));
+lazy_static! {
+    pub static ref _FILE: ::std::sync::Mutex<::std::fs::File>
+        = ::std::sync::Mutex::new(::std::fs::File::create("tmc-files.txt").unwrap());
 }
 
-thread_local!(static FNS_IN_MOD: RefCell<HashMap<Ident, Ident>>
-                = RefCell::new(HashMap::new()));
-thread_local!(static RESULT_FILE: RefCell<File>
-                = RefCell::new(File::create("tmc-points.txt").unwrap()));
-
-fn do_it(_: &mut ExtCtxt,
-         _: Span,
-         meta: &MetaItem,
-         item: &Annotatable,
-         _: &mut FnMut(Annotatable)) {
-    if let Some(value) = resolve_value(meta) { 
-        if let &Item(ref i) = item {
-            match i.node {
-                ItemMod(Mod{ref items, ..}) => {
-                    handle_module(i.ident, value, items);
-                }
-                ItemFn(..) => {
-                    handle_function(&i.ident, value);
-                }
-                _ => {}
-            }
+#[macro_export]
+macro_rules! points {
+    () => {};
+    (#[points = $points:expr] mod $suite:ident { $($block:tt)* } $($tail:tt)*) => {
+        mod $suite {
+            points!(@helper $points, $suite, $($block)*);
         }
-    }
-}
-
-fn handle_module(ident: Ident, value: &str, items: &Vec<P<syntax::ast::Item>>) {
-    for item in items {
-        if let ItemFn(..) = item.node {
-            FNS_IN_MOD.with(|fun| {
-                fun.borrow_mut()
-                   .insert(item.ident, ident);
-            });
+        points!($($tail)*);
+    };
+    (#[points = $points:expr] fn $test:ident() $code:block $($tail:tt)*) => {
+        #[test]
+        fn $test() {
+            use std::io::Write;
+            let mut file = $crate::_FILE.lock().unwrap();
+            write!(file, "{} = {}\n", stringify!($test), $points).unwrap();
+            $code
         }
-    }
-    RESULT_FILE.with(|file| {
-        let mut file_borrow = file.borrow_mut();
-        write!(file_borrow, "{} = {}\n", ident, value).unwrap();
-    });
-}
+        points!($($tail)*);
+    };
 
-fn handle_function(ident: &Ident, value: &str) {
-    FNS_IN_MOD.with(|fun| {
-        RESULT_FILE.with(|file| {
-            let mut file_borrow = file.borrow_mut();
-            if let Some(s) = fun.borrow().get(ident) {
-                write!(file_borrow, "{}.{} = {}\n", s, ident, value).unwrap();
-            } else {
-                write!(file_borrow, "{} = {}\n", ident, value).unwrap();
-            }
-        });
-    });
-}
-
-fn resolve_value(meta: &MetaItem) -> Option<&InternedString> {
-    if let MetaNameValue(_, ref lit) = meta.node {
-        if let LitStr(ref string, _) = lit.node {
-            return Some(string);
+    (@helper $spoints:expr, $suite:ident,) => {};
+    (@helper $spoints:expr, $suite:ident, #[points = $points:expr] fn $test:ident() $code:block) => {
+        #[test]
+        fn $test() {
+            use std::io::Write;
+            let mut file = $crate::_FILE.lock().unwrap();
+            write!(file, "{} = {}\n", stringify!($suite), $spoints).unwrap();
+            write!(file, "{}.{} = {}\n", stringify!($suite), stringify!($test), $points).unwrap();
+            $code
         }
-    }
-    None
+    };
+    (@helper $spoints:expr, $suite:ident, #[points = $points:expr] fn $test:ident() $code:block $($tail:tt)*) => {
+        #[test]
+        fn $test() {
+            use std::io::Write;
+            let mut file = $crate::_FILE.lock().unwrap();
+            write!(file, "{}.{} = {}\n", stringify!($suite), stringify!($test), $points).unwrap();
+            $code
+        }
+        points!(@helper $spoints, $suite, $($tail)*);
+    };
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,28 +1,26 @@
-#![feature(plugin, custom_attribute)]
-#![plugin(points)]
+#[macro_use]
+extern crate points;
 
-#[test]
-#[points = "1"]
-fn first_fn() {
+points! {
+    #[points = "1"]
+    fn first_fn() {
 
-}
-
-#[points = "2"]
-mod first {
-    #[test]
-    #[points = "2.1"]
-    fn second_fn() {
-    
     }
-    #[test]
-    #[points = "2.2"]
-    fn third_fn() {
-    
+
+    #[points = "2"]
+    mod first {
+        #[points = "2.1"]
+        fn second_fn() {
+
+        }
+        #[points = "2.2"]
+        fn third_fn() {
+
+        }
     }
-}
 
-#[test]
-#[points = "3"]
-fn fourth_fn() {
+    #[points = "3"]
+    fn fourth_fn() {
 
+    }
 }
